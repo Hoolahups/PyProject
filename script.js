6506aa0d74595c4c2e561262cf87c795a76662c4
Joseph Cook <the.joseph.cook@gmail.com>
2024-04-30 16:42:04 -0600

    gughh

--- script.js
+++ script.js
@@ -27,3 +27,15 @@ function displayImage(imagePath) {
         document.getElementById('errorMessage').textContent = 'This month\'s data does not exist or invalid selection.';
     };
 }
+
+function updateSelectors() {
+    var type = document.getElementById('type').value;
+    var yearSelector = document.getElementById('year');
+    var monthSelector = document.getElementById('month');
+    
+    if (type === 'gif') {
+        yearSelector.style.display = 'none';
+        monthSelector.style.display = 'none';
+    } else {
+        yearSelector.style.display = 'block';
+        monthSelector.style.display = 'block';
