69ec3ce78c39d602ad0fb1eb7f0ad811d2974c43
Joseph Cook <the.joseph.cook@gmail.com>
2024-05-01 14:11:38 -0600

    what the sigma

--- script.js
+++ script.js
@@ -88,15 +88,4 @@ document.getElementById('folder').addEventListener('change', function() {
 document.getElementById('year').addEventListener('change', updateImage);
 document.getElementById('month').addEventListener('change', updateImage);
 
-document.getElementById('lineNameInput').addEventListener('input', function(e) {
-    let inputVal = e.target.value.toLowerCase();
-    let filteredNames = names.filter(name => name.toLowerCase().includes(inputVal));
-    
-    let dataList = document.getElementById('nameOptions');
-    dataList.innerHTML = ''; // Clear previous options
-    filteredNames.forEach(function(name) {
-        var option = document.createElement('option');
-        option.value = name;
-        dataList.appendChild(option);
-    });
-});
+
